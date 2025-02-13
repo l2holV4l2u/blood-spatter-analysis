7a1637d6eae170fdd9bb7ece4c415f07d9d55bb8
l2ho.lV4l2u <naruesornprabpon@gmail.com>
2025-02-13 19:01:18 +0800

    configure vercel function deployment

--- api/index.py
+++ api/index.py
@@ -9,7 +9,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+from http.server import BaseHTTPRequestHandler
+ 
 class handler(BaseHTTPRequestHandler):
+ 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
